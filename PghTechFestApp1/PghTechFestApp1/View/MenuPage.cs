5fe9d514603d9e2e00802f930a006baacbc31711
yortch <jorgerbf@gmail.com>
2014-06-04 10:16:49 -0400

    Fixed bug for deselecting from main menu

--- PghTechFestApp1/PghTechFestApp1/View/MenuPage.cs
+++ PghTechFestApp1/PghTechFestApp1/View/MenuPage.cs
@@ -9,36 +9,35 @@ namespace PghTechFestApp1.View
 {
     class MenuPage : ContentPage
     {
+
+
         public MenuPage()
         {
-            this.SetValue(Page.TitleProperty, "Pgh Tech Fest 2014");
-            var list = new ListView();
-            list.ItemsSource = new string[] { "Speakers", "Sessions" };
+			Title = "Pgh Tech Fest 2014";
+			ListView list = new ListView ();
+			list.ItemsSource = new string[] { "Speakers", "Sessions" };
 
-            list.ItemSelected += list_ItemSelected;
-            list.SelectedItem = null;
+			ContentPage page = null;
+			list.ItemSelected += (sender, e) => {
+				var item = (String)e.SelectedItem;
+				switch (item) {
+				case "Speakers":
+					page = new SpeakerListPage ();
+					break;
+				case "Sessions":
+					page = new SessionListPage ();
+					break;
+				default:
+					break;
+				}
+				Navigation.PushAsync (page);
+				list.SelectedItem = null;
+			};
             Content = new StackLayout
             {
                 Children = { list }
             };
         }
 
-        void list_ItemSelected(object sender, SelectedItemChangedEventArgs e)
-        {
-            var item = (String)e.SelectedItem;
-            ContentPage page = null;
-            switch (item)
-            {
-                case "Speakers":
-                    page = new SpeakerListPage();
-                    break;
-                case "Sessions":
-                    page = new SessionListPage();
-                    break;
-                default:
-                    break;
-            }
-            Navigation.PushAsync(page);
-        }
     }
 }
